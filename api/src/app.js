2de45d52e4f6352923fad5b668e7a2d1d7fc8bcb
albertovilva <albert.vilva@gmail.com>
2018-12-07 16:02:09 +0100

    Connected API with DB

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
 
-const {PORT} = process.env
+const {PORT, DATABASE} = process.env
 
-console.log('Server running 👍')
+// Connect DB
+mongoose.connect(DATABASE);
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
 
 app.get('/', (req, res) => {
   res.send('hello world!')
 })
 
 app.listen(PORT)
+
+console.log('Server running 👍')
