5cd34618d4ef1311605c97af39291cf2c8365dc5
albertovilva <albert.vilva@gmail.com>
2018-12-09 19:30:19 +0100

    Api: procedurally generating complex sentences

--- api/src/controllers/tattooController.js
+++ api/src/controllers/tattooController.js
@@ -1,26 +1,55 @@
 const subjects = require('../dictionaries/subjects.json')
 const adjectives = require('../dictionaries/adjectives.json')
+const connectors = [
+  'with',
+  'covered with'
+]
+
 
 const getRandomItem = (array) => {
   const randomId = Math.floor(Math.random() * array.length)
   return array[randomId]
 }
 
-const getContentOnChance = (content, chance) => {
+const addArticle = word => {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  const isVowel = vowels.includes(word[0])
+  const article = isVowel ? 'an' : 'a'
+  return `${article} ${word}`
+}
+
+const maybe = (content, chanceToAppear) => {
   const randomNum = Math.random()
-  const chanceDefeated = randomNum > chance
+  const chanceDefeated = randomNum > (1 - chanceToAppear)
   
   return chanceDefeated ? content : ''
 }
 
-const generateIdea = () => {
-  const subject = getRandomItem(subjects)
-  const adjective = getContentOnChance(getRandomItem(adjectives), 0.2)
-
-  return `a ${adjective} ${subject} `
+const cleanSpaces = (string) => {
+  return string.replace(/  /g, ' ')
 }
 
-console.log(generateIdea())
+const getExtraSubject = () => {
+  const connector = getRandomItem(connectors)
+  const subject = generateSubject()
+  return `${connector} ${subject}`
+}
+
+const generateSubject = () => {
+  const subject = getRandomItem(subjects)
+  const adjective = `${maybe(getRandomItem(adjectives), .5)} `
+  const idea = `${adjective}${subject} `
+  return addArticle(idea)
+}
+
+const generateTattooIdea = () => {
+  const mainSubject = generateSubject()
+  const extraSubject = maybe(getExtraSubject(), .2)
+  const idea =  `${mainSubject}${extraSubject}`
+  return cleanSpaces(idea)
+}
+
+console.log(generateTattooIdea())
 
 exports.generateTattoo = (req, res) => {
   res.json({
